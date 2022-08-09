e80a18ae7336a4c87c453b45a280d28f8ee66aba
Berta D <berta.diaz.mtz@gmail.com>
2022-08-09 10:15:55 -0500

    ex 01

--- Day1/ClassExercise/day01_exercise.py
+++ Day1/ClassExercise/day01_exercise.py
@@ -1,11 +1,22 @@
 # Fibonacci sequence
 # X_[i] = X_[i-1] + X_[i-2], where X_1 = 1, X_2 = 1
-# 1,1,2,3,5,8,....
+# 1,1,2,3,5,8,13,21,34,55 
 
 # Write a for loop, while loop, or function (or all three!) to create a
 # list of the first 10 numbers of the fibonacci sequence
 
+def fibonacci(n):
+    x_1=1
+    x_2=1
+    l = [x_1, x_2]
+    for m in range(n-2):
+        #print(m)
+        l.append(l[m]+l[(m+1)])
+        #print(l[m]+l[(m+1)])
+        
+    return(l)
 
+fibonacci(10)
 
 """return true if there is no e in 'word', else false"""
 def has_no_e(word):
