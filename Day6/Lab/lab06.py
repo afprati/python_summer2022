e70fa97b351ebbe412bc0fa874e9136f1b6df569
Berta D <berta.diaz.mtz@gmail.com>
2022-08-15 20:28:02 -0500

    .

--- Day6/Lab/lab06.py
+++ Day6/Lab/lab06.py
@@ -7,16 +7,39 @@
 # you may find this page useful to learn about different findinging nearby places https://www.geeksforgeeks.org/python-fetch-nearest-hospital-locations-using-googlemaps-api/
 
 
-import importlib
+import importlib 
 import os
 import googlemaps
 
-os.chdir('you/key/dir')
+os.chdir('C:\\Users\\Tita\\Documents\\GitHub\\python_summer2022\\Day6\\Lab')
 imported_items = importlib.import_module('start_google')
 gmaps = imported_items.client
 
 whitehouse = '1600 Pennsylvania Avenue, Washington, DC'
+# Locate the white house
+wh_location = gmaps.geocode(whitehouse)
+wh_location # location is a list of dictionaries
 
 embassies = [[38.917228,-77.0522365], 
 	[38.9076502, -77.0370427], 
 	[38.916944, -77.048739] ]
+embassies[0]
+
+# Get keys
+wh_location[0].keys()
+# Get geometry
+wh_location[0]['geometry'].keys()
+# Get location
+wh_location[0]['geometry']['location']
+
+
+# Store latlong
+latlong = wh_location[0]['geometry']['location']
+
+
+# Get the destination using latlong
+destination = gmaps.reverse_geocode(latlong)
+
+locations = ""
+
+
---
+++ Day6/Lab/start_google.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 14:02:59 2022
+
+@author: Tita
+"""
+
+import googlemaps
+
+api_key = "AIzaSyBR98QJUBDpXe5vtI73zkOZPLCzKPY4vVI"
+client = googlemaps.Client(api_key)
